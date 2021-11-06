e6c036ae485b3d2bf9e018c016e5c10881327cdf
Michael Pacher <s52103994@edu.campus02.at>
2021-11-06 09:34:16 +0100

    Tests implementiert Korrekturen

--- src/test/java/at/campus02/pacher/AnimalTest.java
+++ src/test/java/at/campus02/pacher/AnimalTest.java
@@ -6,30 +6,38 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AnimalTest {
 
-    private static Animal animal;
+    private static Animal animal1, animal2, animal3;
+
 
     @BeforeAll
     static void beforeAll() {
-        animal = new Animal("Bella",2,"Hund","Wuff");
+
+        animal1 = new Animal("Bella",1,"Hund","Wuff");
+        animal2 = new Animal("Kitty",5,"Katze","Miau");
+        animal3 = new Animal("Lion",3,"Löwe","Roarr");
     }
 
     @Test
     @DisplayName("Animal loud")
     void giveAnimalLoud(){
-        var loud = animal.giveAnimalLoud();
-        assertEquals("Wuff",loud);
+        assertEquals("Wuff",animal1.giveAnimalLoud());
+        assertEquals("Miau",animal2.giveAnimalLoud());
+        assertEquals("Roarr",animal3.giveAnimalLoud());
     }
 
     @Test
     @DisplayName("Animal name and age")
     void getNameAndAge(){
-        var nameAndAge = animal.getNameAndAge();
-        assertEquals("Bella,2",nameAndAge);
+        assertEquals("Bella,1",animal1.getNameAndAge());
+        assertEquals("Kitty,5",animal2.getNameAndAge());
+        assertEquals("Lion,3",animal3.getNameAndAge());
     }
 
     @Test
     @DisplayName("Animal category")
     void getAnimalCategory(){
-        assertEquals("baby animal", animal.getAnimalCategory());
+        assertEquals("baby animal", animal1.getAnimalCategory());
+        assertEquals("old animal", animal2.getAnimalCategory());
+        assertEquals("young animal", animal3.getAnimalCategory());
     }
 }
